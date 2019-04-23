fd281257fb813642832faa6c7dbbfb0e48bca167
Hannes Hörl <hhorl@pivotal.io>
2019-04-23 14:12:08 +0100

    Create a signing cert and load it into the keychain
    This certificate can be used with `codesign`.
    
    Signed-off-by: Maria Ntalla <mntalla@pivotal.io>

---
+++ create-and-load-cert.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+getConf() {
+cat <<EOF
+[ req ]
+prompt             = no
+distinguished_name = my dn
+
+[ my dn ]
+# The bare minimum is probably a commonName
+commonName = SigningTests
+countryName = UK
+localityName = Longon
+organizationName = ACME
+organizationalUnitName = SSL certs and things
+stateOrProvinceName = none
+emailAddress = hhorl@pivotal.io
+name = Hannes Hoerl
+surname = Hoerl
+givenName = Hannes
+initials = HH
+dnQualifier = some
+
+[ my server exts ]
+keyUsage = digitalSignature
+extendedKeyUsage = codeSigning
+EOF
+}
+
+echo "generating the private key ..."
+openssl genrsa -des3 -passout pass:foobar -out server.key 2048
+
+echo ""
+echo "generating the CSR (certificate signing request) ..."
+openssl req -new -passin pass:foobar -passout pass:foobar -key server.key -out server.csr -config <( getConf ) -extensions 'my server exts'
+
+echo ""
+echo "generating the self-signed certificate ..."
+openssl x509 -req -passin pass:foobar -days 10 -in server.csr -signkey server.key -out server.crt -extfile <( getConf ) -extensions 'my server exts'
+
+echo ""
+echo "convert crt + RSA private key into a PKCS12 (PFX) file ..."
+openssl pkcs12 -export -passin pass:foobar -passout pass:foobar -in server.crt -inkey server.key -out server.pfx
+
+echo ""
+echo "importing the certificate ..."
+security import server.pfx -P foobar
